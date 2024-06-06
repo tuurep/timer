9b355cffe7958ff219ebb238f410fb97730c7301
Tuure <tuure.piitulainen@gmail.com>
2024-06-07 02:47:33 +0300

    Experiment with `blessed` library
    This could work if it can:
        * cleanly refresh an area of a few lines
        * handle keypresses while timer is running

--- timer.py
+++ timer.py
@@ -2,11 +2,15 @@
 
 import sys
 import time
+import blessed
 
 seconds = int(sys.argv[1])
+term = blessed.Terminal()
 
 for s in range(seconds, 0, -1):
-    print(s)
+    sys.stdout.write(str(s))
+    sys.stdout.flush()
     time.sleep(1)
+    sys.stdout.write(term.clear_bol + term.move_x(0))
 
-print("0 ✓")
+sys.stdout.write("0\n")
