9af6f4b942de77c737deaec2c127d0d65437c825
Tuure <tuure.piitulainen@gmail.com>
2024-06-07 17:01:36 +0300

    Don't buffer or echo input to stdout

--- timer.py
+++ timer.py
@@ -3,14 +3,17 @@
 import sys
 import time
 import blessed
+from functools import partial
+
+echo = partial(print, end="", flush=True)
 
 seconds = int(sys.argv[1])
 term = blessed.Terminal()
 
-for s in range(seconds, 0, -1):
-    sys.stdout.write(str(s))
-    sys.stdout.flush()
-    time.sleep(1)
-    sys.stdout.write(term.clear_bol + term.move_x(0))
+with term.cbreak():
+    for s in range(seconds, 0, -1):
+        echo(s)
+        time.sleep(1)
+        echo(term.clear_bol + term.move_x(0))
 
-sys.stdout.write("0\n")
+echo("0\n")
