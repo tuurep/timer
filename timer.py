79a851a019590d2b09d54c3fb02c2bac0a68fccc
Tuure <tuure.piitulainen@gmail.com>
2024-06-08 01:36:08 +0300

    Format h:m:s like intended
    Hours only shown if there are any,
    if not, minutes don't have leading zero.

--- timer.py
+++ timer.py
@@ -6,14 +6,27 @@ import blessed
 from functools import partial
 
 echo = partial(print, end="", flush=True)
-
-seconds = int(sys.argv[1])
 term = blessed.Terminal()
 
+total_seconds = int(sys.argv[1])
+
 with term.cbreak():
-    for s in range(seconds, 0, -1):
-        echo(f"\n  {s}\n\n")
+    for seconds_left in range(total_seconds, 0, -1):
+        h = seconds_left // 3600
+        m = seconds_left % 3600 // 60
+        s = seconds_left % 60
+
+        echo("\n  ")
+
+        if (h > 0):
+            echo(f"{h}:{m:02}:{s:02}")
+        else:
+            echo(f"{m}:{s:02}")
+
+        echo("\n\n")
+
         time.sleep(1)
+
         echo(term.move_up(3) + term.move_x(0) + term.clear_eos())
 
-echo("\n  0\n\n")
+echo("\n  0:00\n\n")
