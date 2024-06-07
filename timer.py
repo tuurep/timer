1a8409968f2d23df5bc496b1da5d7a19b18794ce
Tuure <tuure.piitulainen@gmail.com>
2024-06-07 17:38:12 +0300

    Add padding for the countdown area

--- timer.py
+++ timer.py
@@ -12,8 +12,8 @@ term = blessed.Terminal()
 
 with term.cbreak():
     for s in range(seconds, 0, -1):
-        echo(s)
+        echo(f"\n  {s}\n\n")
         time.sleep(1)
-        echo(term.clear_bol + term.move_x(0))
+        echo(term.move_up(3) + term.move_x(0) + term.clear_eos())
 
-echo("0\n")
+echo("\n  0\n\n")
