cbcb0cb8c54e34421e9ba1a1e7734ab29cae99ba
Tuure <tuure.piitulainen@gmail.com>
2024-06-10 20:38:55 +0300

    Accept input format "1h30m30s" and similar

--- timer.py
+++ timer.py
@@ -14,7 +14,7 @@ def parse_input_to_seconds():
     input = sys.argv[1]
 
     # h:mm:ss
-    match = re.match("^(1?[0-9]|2[0-4])[:.]([0-5][0-9])[:.]([0-5][0-9])$", input)
+    match = re.match("^(1?[0-9]|2[0-3])[:.]([0-5][0-9])[:.]([0-5][0-9])$", input)
     if match:
         h, m, s = match.groups()
         return int(h)*60*60 + int(m)*60 + int(s)
@@ -26,12 +26,24 @@ def parse_input_to_seconds():
         return int(m)*60 + int(s)
 
     # m
-    match = re.match("^(60|[1-5]?[0-9])$", input)
+    match = re.match("^([1-5]?[0-9])$", input)
     if match:
         m = match.group(1)
         return int(m) * 60
 
-    # todo: accept format like "1h30m30s", "1h", "5s" ...
+    # "1h30m30s", "2h", "50s" ...
+    match = re.match("^(?:(1?[0-9]|2[0-3])h)?(?:([1-5]?[0-9])m)?(?:([1-5]?[0-9])s)?$", input)
+    if match and any(match.groups()):
+        h, m, s = match.groups()
+        h = h or "0"
+        m = m or "0"
+        s = s or "0"
+        return int(h)*60*60 + int(m)*60 + int(s)
+
+    # upper limit 24h
+    match = re.match("^(24h|24[:.]00[:.]00)$", input)
+    if match:
+        return 24*60*60
 
     # todo: handle invalid args
     return -1
